a83cc393bfe697e3b8c87fde1d629a8c1eee3394
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-04-23 12:55:16 -0500

    Add files via upload

---
+++ model/DataSource.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSource {
+    public static final String DB_NAME = "music.db";
+
+    //connection string
+    public static final String CONNECTION_STRING = "jdbc:sqlite:/Users/elenavoinu/OneDrive - City Colleges of Chicago/udemy/MusicSQLite/" + DB_NAME;
+
+    //table albums
+    public static final String TABLE_ALBUMS = "albums";
+    public static final String COLUMN_ALBUM = "_id";
+    public static final String COLUMN_ALBUM_NAME = "name";
+    public static final String COLUMN_ALBUM_ARTIST = "artist";
+
+    //table artists
+    public static final String TABLE_ARTISTS = "artists";
+    public static final String COLUMN_ARTIST_ID = "_id";
+    public static final String COLUMN_ARTIST_NAME = "name";
+
+    //table songs
+    public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONG_TRACK = "track";
+    public static final String COLUMN_SONG_TITLE = "title";
+    public static final String COLUMN_SONG_ALBUM = "album";
+
+
+    //establish the connection
+    private Connection connection;
+    public boolean open(){
+        try{
+            connection= DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Couldn't connect to the database " +  e.getMessage());
+
+            return false;
+        }
+    }
+
+    //close the connection
+    public void close(){
+        try{
+            if(connection!=null){
+                connection.close();
+            }
+        }
+        catch(SQLException e){
+            System.out.println("Couldn't close connection " + e.getMessage());
+        }
+    }
+
+
+    //return a list of artists
+
+
+
+
+
+}
