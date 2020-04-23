f6c664527080ad85db3f974fb4485b720815406f
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-04-23 12:58:42 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+//Elena Voinu 2020
 import model.DataSource;
 
 public class Main {
     public static void main(String[] args) {
+        
 
+        //create instance of datasource
         DataSource dataSource = new DataSource();
 
         if(!dataSource.open()){
